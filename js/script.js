7fa9a86fdc6af2f374d73abbeb4ea0b89a62433c
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2021-04-04 12:33:20 +0200

    commit 2
    -mise en place du système de caméra
    -ajout du dount( sans les matériaux)

--- js/script.js
+++ js/script.js
@@ -1,10 +1,38 @@
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.new THREE.MeshBasicMaterial( { color : 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
+var scene = new THREE.Scene();
+scene.background = new THREE.Color( 0xd238a5 );
+
+var camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 10000);
+
+var controls = new THREE.OrbitControls( camera, renderer.domElement );
+
+
+
+camera.position.set(0, 20, 100);
+controls.update();
+window.addEventListener('resize',function(){
+  renderer.setSize(window.innerWidth,window.innerHeight);
+  camera.aspect=window.innerWidth/window.innerHeight
+})
+
+
+
+var loader = new THREE.GLTFLoader();
+
+loader.load('../model/scene.glb', function( gltf ){
+  scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+  console.error( error );
+});
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene,camera);
+}
+
+animate();
