78b1c5207608f99cee9a70cecc98a86a9aa5dcc6
P4tt4te <57622377+P4tt4te@users.noreply.github.com>
2021-04-04 15:08:12 +0200

    commit 3
    -ajout d'une lumière pour l'éclairage du donut
    -caméra plus proche

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@ document.body.appendChild(renderer.domElement);
 var scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xd238a5 );
 
-var camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 10000);
+var camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 7000);
 
 var controls = new THREE.OrbitControls( camera, renderer.domElement );
 
 
 
-camera.position.set(0, 20, 100);
+camera.position.set(0, 0, 10);
 controls.update();
 window.addEventListener('resize',function(){
   renderer.setSize(window.innerWidth,window.innerHeight);
@@ -28,6 +28,9 @@ loader.load('../model/scene.glb', function( gltf ){
 }, undefined, function ( error ) {
   console.error( error );
 });
+
+var light = new THREE.AmbientLight( 0xffffff );
+scene.add(light);
 
 function animate() {
   requestAnimationFrame(animate);
